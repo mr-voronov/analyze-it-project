ccf0c904e5ba8c0a0972485efc045f31497fc5e5
User <mr.denis.voronov@gmail.com>
2022-05-05 16:56:32 +0300

    file with data variables added

---
+++ dataVariables.js
@@ -0,0 +1,28 @@
+// obj to combine all the lists with data to compare
+
+const dataToCompare = {
+    "houseArrest": {
+        "textName": "* Домашній арешт",
+        "variable": houseArrestData,
+    },
+    "domesticAbuser": {
+        "textName": "* Кривдник",
+        "variable": domesticAbuserData,
+    },
+    "convicted": {
+        "textName": "* Раніше засуджені",
+        "variable": convictedData,
+    },
+    "wanted": {
+        "textName": "* Розшук",
+        "variable": wantedData,
+    },
+    "refugees1": {
+        "textName": "Біженці 1",
+        "variable": refugeesData1,
+    },
+    "refugees2": {
+        "textName": "Біженці 2",
+        "variable": refugeesData2,
+    },
+};
